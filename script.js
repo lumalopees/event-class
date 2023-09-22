fdcdbf0db2ee01791e1e26d3cb7cc08495917125
Luma <luuh_lopees@live.com>
2023-09-22 19:35:43 -0300

    :sparkles: 20min | tá indo até rápido

--- script.js
+++ script.js
@@ -5,4 +5,8 @@ window.onload = () => {
   const player1 = document.getElementById('player1')
   const player2 = document.getElementById('player2')
   const winnerSong = document.getElementById('audioWinner')
+
+  const containerPlayers = document.getElementsByClassName('car-section')
+
+  containerPlayers.addEventListener()
 }
